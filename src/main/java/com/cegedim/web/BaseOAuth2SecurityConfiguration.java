f2bd93c1101bbf3fef146e022444dd9217035330
harold <harold@dev01>
2019-07-29 00:07:33 +0200

    Fixed sonar and authentication token problems and deferred

--- src/main/java/com/cegedim/web/BaseOAuth2SecurityConfiguration.java
+++ src/main/java/com/cegedim/web/BaseOAuth2SecurityConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
