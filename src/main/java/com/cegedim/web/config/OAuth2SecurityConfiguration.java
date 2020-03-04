cace22a3e117511ed7e7cfce58ed4fa9a8c8bb7d
harold <harold@dev01>
2020-03-04 23:03:41 +0100

    Fix refresh token

--- src/main/java/com/cegedim/web/config/OAuth2SecurityConfiguration.java
+++ src/main/java/com/cegedim/web/config/OAuth2SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.cegedim.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 import com.gvt.web.security.BaseOAuth2SecurityConfiguration;
-import com.gvt.web.security.filters.TokenValidatorFilter;
 import com.gvt.web.security.signature.DefaultJWTSecretKeyFactory;
 import com.gvt.web.security.signature.JWTSecretKeyFactory;
 
@@ -26,29 +19,9 @@ import com.gvt.web.security.signature.JWTSecretKeyFactory;
 //@Order(3)
 public class OAuth2SecurityConfiguration extends BaseOAuth2SecurityConfiguration {
 
-	@Value("${security.oauth2.client.logoutUri}")
-	private String logoutUrl;
-
-	@Autowired
-	private OAuth2ClientContextFilter oauth2ClientContextFilter;
-
-	@Value("${security.oauth2.client.accessTokenUri}")
-	private String accessTokenUri;
-
-	@Autowired
-	private OAuth2ClientContext oAuth2ClientContext;
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.headers().frameOptions().disable().and().cors().and().csrf().disable().authorizeRequests()
-				.antMatchers("/login").permitAll().anyRequest().authenticated().and().logout()
-				.logoutSuccessUrl(logoutUrl).and().anonymous().disable()
-				.addFilterAfter(oauth2ClientContextFilter, SecurityContextPersistenceFilter.class)
-				.addFilterAfter(new TokenValidatorFilter(accessTokenUri, oAuth2ClientContext, oauth2RestTemplate),
-						BasicAuthenticationFilter.class)
-				// this disables session creation on Spring Security
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
-//		.invalidSessionStrategy(new CustomRedirectStrategy("/logout"));
+		super.configure(http);
 
 //		http.authorizeRequests().anyRequest().authenticated();
 	}
